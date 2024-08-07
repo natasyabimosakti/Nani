3d6c5b6f87bdcd8caa5d06d6764736e7861440cf
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-07 10:22:25 +0700

    Update Nani1.js

--- Nani/Nani1.js
+++ Nani/Nani1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         POLOS
 // @namespace    http://tampermonkey.net/
-// @version      3.09
+// @version      3.10
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Nani/main/Nani/Nani1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Nani/main/Nani/Nani1.js
