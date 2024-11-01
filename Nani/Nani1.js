18c38073ada57cae3be810364e1fe3b5be2deda2
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-02 02:21:01 +0700

    Update Nani1.js

--- Nani/Nani1.js
+++ Nani/Nani1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         POLOS
 // @namespace    http://tampermonkey.net/
-// @version      3.27
+// @version      3.28
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Nani/main/Nani/Nani1.js?token=GHSAT0AAAAAACVY7DN4K7FY4LMSOW7GCVD4ZVTRX4Q
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Nani/main/Nani/Nani1.js?token=GHSAT0AAAAAACVY7DN4K7FY4LMSOW7GCVD4ZVTRX4Q
@@ -52,8 +52,8 @@ var Comment11 = '#TOYIBSLOT ( TANLOL87 ) : 65*56*99';
 var namagroup12 = 'MASTER KUY';
 var Comment12 = 'TOGELKUY TANLANAS36 14*57*99'; 
 
-var namagroup13 = 'L0MBA';
-var Comment13 = '#TOTO4D (TANLOL87) 37*15*50'; 
+var namagroup13 = 'TASIKTOTO';
+var Comment13 = '#TASIKTOTO(TANLOL87) = 37*15'; 
 
 var namagroup14 = 'DENTOTO';
 var Comment14 = '#DENTOTO 36*80*62 ( TANLES78 )'; 
@@ -94,7 +94,7 @@ var id16 = await GM.getValue(16);
 var id17 = await GM.getValue(17);
 var id18 = await GM.getValue(18);
 
-var admin = ["Siâo"," ","andre","adiat","andy","ayunda","audi","arxidi","aditia","aldi","ananda","alde","adm","ayesha",
+var admin = ["Siâo"," ","andre","adiat","andy","ayunda","Ozawa","audi","arxidi","aditia","aldi","ananda","alde","adm","ayesha",
              "boleng","biru","bobby","bastian",
              "cristina","camb","cassa","che","cinta","celsia",
              "david",",dewa","desi","debby","dewi","dentoto","dika",
