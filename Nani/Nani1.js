39af146e5f6c9f5c611733828e330b77424519dd
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-10 06:55:33 +0700

    Update Nani1.js

--- Nani/Nani1.js
+++ Nani/Nani1.js
@@ -28,7 +28,7 @@ var Comment3 = 'Tiktaktogel / TANLOYO12 / 36 , 80 , 62';
 var namagroup4 = 'GAIB';
 var Comment4 = 'GAIB4D=TANKOR90=37*15*50'; 
 
-var namagroup5 = 'KEITOGEL';
+// @version      3.NaN
 var Comment5 = '#keitogel = (TANMO38) = 14*57*42'; 
 
 var namagroup6 = 'Pasar';
@@ -94,27 +94,28 @@ var id16 = await GM.getValue(16);
 var id17 = await GM.getValue(17);
 var id18 = await GM.getValue(18);
 
-var admin = ["Siâo"," ","andre","adiat","andy","ayunda","Ozawa","audi","arxidi","aditia","aldi","ananda","alde","adm","ayesha",
-             "boleng","biru","bobby","bastian",
-             "cristina","camb","cassa","che","cinta","celsia",
-             "david",",dewa","desi","debby","dewi","dentoto","dika",
-             "erwin",
-             "fira","fahresa",
-             "gita",
-             "habib","hefi","hoihai",
-             "icha","iyatoto",
-             "jordi","jaguar","jne",
-             "keitogel","kumbara","kembar","kotna",
-             "lianda","lusiana","lina","laura",
-             "mahendra","monica","mey","mersya","mad","multi","mariana","melati",
-             "nasution","nyocol","naura","neng","nino","nona","neman","novi","nella",
-             "oscar",
-             "pung","puput","priyan","primus","primus",
-             "ratu","rio","ria","rikodo","rizal","roy","rendy",
-             "sandiego","san","sanjaya","siska","safar","sinta","surianti","satria","sapto","salsabila","sanchez","sofia","sonia","serena",
+var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi","ananda","alde","adm","ayesha","aqisya","anjani","apri","amore","arifin","ayunda","agung","arem",
+             "boleng","biru","bobby","bastian","bambang","bogard",
+             "cristina","camb","cassa","che","cinta","celsia","calista","cahyo","cipto","claura","chelsea","calista",
+             "david",",dewa","desi","debby","dewi","dentoto","dika","delon",
+             "erwin","elvina",
+             "fira","fahresa","findlay",
+             "gita","genzo",
+             "habib","hefi","hoihai","herfizah",
+             "icha","iyatoto","intan",
+             "jordi","jaguar","jne","je pe","jess","junior","jovanka","jasmine",
+             "kei","kumbara","kembar","kotna","karina",
+             "lianda","lusiana","lina","laura","lia",
+             "mahendra","monica","mey","mersya","mad","multi","mariana","melati","meleqq","megaways","minion","melly","monicha","manu","maryam",
+             "nasution","nyocol","naura","neng","nino","nona","neman","novi","nella","nayla","naomi",
+             "oscar","ozawa",
+             "pung","puput","priyan","primus","primus","prediction",
+             "ratu","rio","ria","rikodo","rizal","roy","rendy","rahma","ratsa",
+             "sandiego","san","sanjaya","siska","safar","sinta","surianti","satria","sapto","salsabila","sanchez","sofia","sonia","serena","specialis","seojun","saskia","sifa",
              "tink","tiktak","tiara","tatang",
-             "yanty","yoky","yohana",
-             "wulan","wok"];
+             "yanty","yoky","yohana","yura","yaya",
+             "wulan","wok","wak",
+             "zuko"];
 
 var keyword1 = "ROOM"
 var keyword2 = "𝗥𝗢𝗢𝗠"
